f856982436d3ccb1e0ecdff310bdeed2329625a6
cklxx <1293822641@qq.com>
2022-07-06 23:46:15 +0800

    feat(css): 增加css style loader

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>"webpack!"</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+  div {
+    color: yellow;
+  }
+</style>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import  bar from "./bar";
+import { createApp } from 'vue' 
+import App from './App.vue' 
+
 const foo =  require('./foo');
 foo.sayHello();
+console.log('[bar]:',bar)
+
+
+const app = createApp(App) 
+app.mount('#app') 
